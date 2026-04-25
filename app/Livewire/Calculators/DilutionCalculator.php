79754a2dee73c653ebfc56d020eca594210712b3
Rana Bilal Munawar <121597965+123q256@users.noreply.github.com>
2026-04-26 01:29:13 +0500

    asdasd

---
+++ app/Livewire/Calculators/DilutionCalculator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Calculators;
+
+use Livewire\Component;
+
+class DilutionCalculator extends Component
+{
+    public function render()
+    {
+        return view('livewire.calculators.dilution-calculator');
+    }
+}
---
+++ app/Livewire/Calculators/MoleRatioCalculator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Calculators;
+
+use Livewire\Component;
+
+class MoleRatioCalculator extends Component
+{
+    public function render()
+    {
+        return view('livewire.calculators.mole-ratio-calculator');
+    }
+}
